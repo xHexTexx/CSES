f44ad3e682a023c71d07397f3ca15b4761daa9bf
Meaw_zap <tee.hemjinda@gmail.com>
2022-05-05 23:47:08 +0700

    .

---
+++ Sorting and Searching/Apartments.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int n , m , k , x ;
+set<int>sett ;
+
+int main(){
+
+    //Solve by HexTex 
+    ios_base :: sync_with_stdio(0) , cin.tie(0) ;
+
+    cin >> n >> m >> k ; 
+
+    for(int i = 0 ; i < n ; i ++ ){
+        cin >> x ;
+        sett.insert(x) ;
+    }
+
+    int res = 0 , cnt ;
+    
+    return 0 ;
+}
